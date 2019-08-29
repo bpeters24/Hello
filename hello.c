1fdfebc057057832608b87eb1d921598f1dbf005
Logan <logan.puatu@icloud.com>
2019-08-29 16:02:42 -0500

    Added collab
    Added collaborator's name and major

--- hello.c
+++ hello.c
@@ -5,5 +5,9 @@ int main(int argc, char **argv) {
 
   printf("Brandon Peters\n");
   printf("Computer Engineering\n");
+  printf("Logan Puatu\n");
+  printf("Computer Science\n");
+
+
   return 0;
 }
